76cbe8ee09a57e7df20f7d5ce3856ec13b9b10bf
Tiziano Müller <tm@dev-zero.ch>
2013-09-27 16:09:17 +0200

    Bump version to 1.2.18.6

--- vm_config.php
+++ vm_config.php
@@ -72,7 +72,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		'virtualization' => array(
-			'version' => '1.2.18.5',
+			'version' => '1.2.18.6',
 
 			'disk' => array(
 				'sstSourceName' => array(
