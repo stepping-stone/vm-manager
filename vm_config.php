f415e99fc602be2f43185a42040a72d9b5d1b16d
Tiziano Müller <tiziano.mueller@stepping-stone.ch>
2013-11-29 09:18:26 +0100

    Bump version to 1.2.18.9

--- vm_config.php
+++ vm_config.php
@@ -72,7 +72,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		'virtualization' => array(
-			'version' => '1.2.18.8',
+			'version' => '1.2.18.9',
 
 			'disk' => array(
 				'sstSourceName' => array(
