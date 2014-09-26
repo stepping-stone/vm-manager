2858d1de2da910ede43a40c715ab16e1e270b238
Pascal Jufer <pascal.jufer@stepping-stone.ch>
2014-09-26 23:44:47 +0200

    Add webdav channel for spice
    The spice webdav channel is now defined in the domain xml.

--- protected/components/CPhpLibvirt.php
+++ protected/components/CPhpLibvirt.php
@@ -301,8 +301,12 @@ class CPhpLibvirt {
 		<channel type=\"spicevmc\">
 			<target type=\"virtio\" name=\"com.redhat.spice.0\"/>
 		</channel>
-        <channel type=\"unix\">
-            <source mode=\"bind\"/>
+		<channel type=\"spiceport\">
+		    <target type=\"virtio\" name=\"org.spice-space.webdav.0\"/>
+		    <source channel=\"org.spice-space.webdav.0\"/>
+		</channel>
+		<channel type=\"unix\">
+			<source mode=\"bind\"/>
 			<target type=\"virtio\" name=\"org.qemu.guest_agent.0\"/>
 		</channel>
 		<video>
