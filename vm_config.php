ab3069acefa447ad9fba747db663f2d6627a8b1a
Tiziano Müller <tiziano.mueller@stepping-stone.ch>
2014-10-08 13:50:09 +0200

    Bump version 1.2.18.16

--- vm_config.php
+++ vm_config.php
@@ -72,7 +72,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		'virtualization' => array(
-			'version' => '1.2.18.15',
+			'version' => '1.2.18.16',
 
 			// Disk specific settings.
 			'disk' => array(
